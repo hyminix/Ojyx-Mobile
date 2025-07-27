56fc8d502c352b9f20117d899965e532076558e8
Hyminix <y.arrighi@gmail.com>
2025-07-27 10:14:33 +0200

    chore: purge massive des fichiers superflus - suppression tests, CI/CD, docs

--- integration_test/app_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:integration_test/integration_test.dart';
-import 'migration_validation_test.dart' as migration_tests;
-
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  // Run all integration test suites
-  migration_tests.main();
-}
